ac1bb5c1a3edfea146b9f9e7aeb045f7cc766f90
Rouse, Michael W. (S&amp;T-Student) <mwrb7d@mst.edu>
2016-03-12 08:07:12 -0600

    Changed it to be a library

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # DOM Elements as Links
-This little JavaScript snippet allows you to have any element on a page act as a link, even if it is not an anchor tag.
+This little JavaScript library allows you to have any element on a page act as a link, even if it is not an anchor tag.
 
 ## Syntax
 The syntax for using any DOM Element as a link is extremely simple, all you do is add an href attribute to the element, and an option target attribute, just like you do on &lt;a&gt; tags.
@@ -24,10 +24,30 @@ a:hover,
 ```
 Yes, this is a bit redundant, seeing as all you need is the [href] selector, but oh well.
 
+## Usage
+To use this library, simply include the source code in your program, or copy and paste it into an existing JavaScript file you have, then run the function
+```javascript
+window.DOMElementsAsLinks();
+```
+when the page is done loading. It is important that it is ran when the page has finished loading, because all DOM elements need to be ready for it to read them.
+
+You can do this by one of the following ways:
+```javascript
+window.onload = function(){
+  window.DOMElementsAsLinks();
+};
+
+```
+```javascript
+window.addEventListener('load', function(){
+  window.DOMElementsAsLinks();
+});
+```
+You should know how to do that though.
+
 ## Example
 [View a CodePen Example](http://codepen.io/mwrouse/pen/aNZxJJ/)
 
 
-## License 
+## License
 Distributed under the [MIT license](https://raw.githubusercontent.com/mwrouse/dom-elements-as-links-javascript/master/LICENSE).
-
--- src/DOMElementLinks.js
+++ src/DOMElementLinks.js
@@ -1,6 +1,6 @@
 /**
  * ====================================
- *     DOMElementLinks
+ *     DOMElementsAsLinks
  * ===================================
  *
  * Author.......: Michael Rouse
@@ -10,57 +10,59 @@
  * Description..: This JavaScript library allows you to use an HTML DOM Element as a link, not just anchor tags. For those times where putting an anchor tag around something might be
  *                annoying, or impossible, for some reason...
  */
+(function(){
+  var DOMElementsAsLinks = function(){
+    /**
+     * Function.....: Register Links (Immediate Function)
+     * Author.......: Michael Rouse
+     * Description..: Registers all the non-anchor tag links on the page
+     */
+    this.register = function(){
+      // Preserve 'this'
+      var me = this;
+
+      // Find all elements with a href attribute that are not <a> tags in the body
+      var elements = document.querySelectorAll('body [href]:not(a)');
+
+      // Loop through each element found
+      for (var i = 0; i < elements.length; i++)
+      {
+        // Register the click event
+        (elements[i]).addEventListener('click', function(){
+          me.follow(this);
+        });
+
+        // Set the title attribute
+        (elements[i]).setAttribute('title', (elements[i]).getAttribute('href'));
+      }
+    };
 
 
-/**
- * Function.....: Register Links
- * Author.......: Michael Rouse
- * Description..: Registers all the non-anchor tag links on the page
- */
-function DOMElementLinks_RegisterLinks()
-{
-  // Find all the elements with an href tag that are not <link> or <a> tags
-  var elements = document.querySelectorAll('[href]:not(a):not(link)');
+    /**
+     * Function.....: Follow Link
+     * Author.......: Michael Rouse
+     * Parameters...: link_element - the element that was clicked
+     * Description..: This function is called when an element with a href attribute that is not a <a> or a <link> tag is clicked
+     */
+    this.follow = function(element){
+      var url = element.getAttribute('href');
 
-  // Loop through each element found
-  for (var i = 0; i < elements.length; i++)
+      if (url)
+      {
+        window.open(url, (element.getAttribute('target') || '_self'));
+      }
+    };
+
+
+    // Constructor, register all the links
+    this.register();
+
+    return this;
+  };
+
+  // Initialize the library
+  if (!window.DOMElementsAsLinks)
   {
-    // Register the click event
-    (elements[i]).addEventListener('click', function(){
-      DOMElementLinks_FollowLink(this);
-    });
-
-    // Add a title attribute to substitute the URL showing in the bottom left of a window
-    (elements[i]).setAttribute('title', (elements[i]).getAttribute('href'));
-
+    window.DOMElementsAsLinks = DOMElementsAsLinks;
   }
-}
-
-
-/**
- * Function.....: DOMElementLinks_FollowLink
- * Author.......: Michael Rouse
- * Parameters...: link_element - the element that was clicked
- * Description..: This function is called when an element with a href attribute that is not a <a> or a <link> tag is clicked
- */
-function DOMElementLinks_FollowLink(link_element)
-{
-  // Pull all the attributes from the element
-  var url = link_element.getAttribute('href');
-  var target = link_element.getAttribute('target') || '_self';
-
-  // Only follow the link if there is a URL entered
-  if (url)
-  {
-    window.open(url, target);
-  }
-}
-
-
-
-/*
- * Register links when the page loads
- */
-window.addEventListener('load', function(){
-  DOMElementLinks_RegisterLinks();
-});
+}());
